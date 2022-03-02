5ac97c94ab2ba42821c7e1e17ca196f94bf82bd0
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-03-01 19:37:46 -0500

    Getting it to work with one for now

--- spinConfig/submit/pre-exec.sh
+++ spinConfig/submit/pre-exec.sh
@@ -3,5 +3,12 @@
 # Remove all old configs
 rm -rf /etc/condor/config.d/*
 
+# If variable is set and the file does not exist make a new "Random" password
+if [[ ! -f "${PASSWORDFILE}" ]]; then
+    uuidgen -r | sed 's/-//g' >> ${PASSWORDFILE}
+    cat ${PASSWORDFILE}
+fi
+
+
 # Write our new configs from environment
 python3 /root/config/startup.py
--- spinConfig/submit/startup.py
+++ spinConfig/submit/startup.py
@@ -1,61 +1,7 @@
 import os
 
 config_template = """# Config created by /root/config/startup.py
-MASTER_NAME = {USERNAME}
-SCHEDD_NAME = {USERNAME}
-CONDOR_HOST = {HOSTNAME}
-COLLECTOR_HOST = $(CONDOR_HOST):{PORT}
-
-USE_SHARED_PORT = True
-SHARED_PORT_ARGS = -p {PORT}
-
-SHARED_PORT_PORT = {PORT}
-
-UID_DOMAIN = {USERNAME}-condor
-DAEMON_LIST = MASTER, SCHEDD
-
-START = True
-SUSPEND = False
-PREEMPT = False
-KILL = False
-
-# Machine resource settings
-NUM_SLOTS = 6
-
-PRIVATE_NETWORK_NAME = $(FULL_HOSTNAME)
-TCP_FORWARDING_HOST = $(CONDOR_HOST)
-
-
-SEC_PASSWORD_FILE = {PASSWORDFILE}
-SEC_DEFAULT_AUTHENTICATION_METHODS = PASSWORD, FS
-
-ALLOW_READ = 128.55.*, 10.*
-ALLOW_WRITE = 128.55.*, 10.*
-
-ALLOW_ADVERTISE_STARTD = $(ALLOW_WRITE)
-ALLOW_ADVERTISE_SCHEDD = $(ALLOW_WRITE)
-ALLOW_WRITE_STARTD = $(ALLOW_WRITE)
-ALLOW_READ_STARTD = $(ALLOW_WRITE)
-ALLOW_WRITE_SCHEDD = $(ALLOW_WRITE)
-ALLOW_READ_SCHEDD = $(ALLOW_WRITE)
-ALLOW_ADVERTISE_MASTER = $(ALLOW_WRITE)
-
-NEGOTIATOR.ALLOW_READ = $(ALLOW_READ)
-NEGOTIATOR.ALLOW_WRITE = $(ALLOW_WRITE)
-
-ALLOW_NEGOTIATOR = *
-
-QUEUE_SUPER_USERS = root, condor, condor_pool, {USERNAME}
-
-COLLECTOR_DEBUG = D_FULLDEBUG
-NEGOTIATOR_DEBUG = D_FULLDEBUG
-MATCH_DEBUG = D_FULLDEBUG
-SCHEDD_DEBUG = D_FULLDEBUG
-"""
-
-cori_conf = """########################################
-# Config created by /root/config/startup.py
-CONDOR_HOST = {HOSTNAME}:{PORT}
+CONDOR_HOST = {CENTRALMANAGER}:{PORT}
 COLLECTOR_HOST = $(CONDOR_HOST)?sock=collector
 
 FILESYSTEM_DOMAIN = $(HOSTNAME)
@@ -64,9 +10,50 @@ UID_DOMAIN = $(HOSTNAME)
 USE_CCB = True
 CCB_ADDRESS = $(CONDOR_HOST)
 
-DAEMON_LIST = MASTER, STARTD
+DAEMON_LIST = MASTER, SCHEDD
 
 QUEUE_ALL_USERS_TRUSTED = True
+
+START = True
+SUSPEND = False
+PREEMPT = False
+KILL = False
+
+# Machine resource settings
+use feature:PartitionableSlot
+
+USE_SHARED_PORT = True
+
+SEC_PASSWORD_FILE = {PASSWORDFILE}
+SEC_DEFAULT_AUTHENTICATION_METHODS = PASSWORD, FS
+
+ALLOW_READ = 128.55.*, 10.*
+ALLOW_WRITE = 128.55.*, 10.*
+ALLOW_WRITE_SCHEDD = $(ALLOW_WRITE)
+ALLOW_READ_SCHEDD = $(ALLOW_WRITE)
+
+SCHEDD_DEBUG = D_FULLDEBUG
+COLLECTOR_DEBUG = D_FULLDEBUG
+NEGOTIATOR_DEBUG = D_FULLDEBUG
+MATCH_DEBUG = D_FULLDEBUG
+SCHEDD_DEBUG = D_FULLDEBUG
+"""
+
+cori_conf = """########################################
+# Config created by /root/config/startup.py
+# Host and port
+CONDOR_HOST = {CENTRALMANAGER}:{PORT}
+COLLECTOR_HOST = $(CONDOR_HOST)?sock=collector
+
+USE_CCB = True
+CCB_ADDRESS = $(CONDOR_HOST)
+USE_SHARED_PORT = True
+
+FILESYSTEM_DOMAIN = $(HOSTNAME)
+UID_DOMAIN = $(HOSTNAME)
+QUEUE_ALL_USERS_TRUSTED = True
+
+DAEMON_LIST = MASTER, STARTD
 
 
 START = True
@@ -75,10 +62,8 @@ PREEMPT = False
 KILL = False
 
 
-# Machine resource settings
 use feature:PartitionableSlot
 
-USE_SHARED_PORT = True
 
 LOCAL_DIR = /global/cscratch1/sd/{USERNAME}/condor/$(HOSTNAME)
 RELEASE_DIR = /global/common/software/m3792/htcondor
@@ -98,9 +83,50 @@ SCHEDD_DEBUG = D_FULLDEBUG
 ########################################"""
 
 
+starting_point = """
+CONDOR_HOST = centralmanager-loadbalancer.htcondor.development.svc.spin.nersc.org:5432
+COLLECTOR_HOST = $(CONDOR_HOST)?sock=collector
+
+FILESYSTEM_DOMAIN = $(HOSTNAME)
+UID_DOMAIN = $(HOSTNAME)
+
+USE_CCB = True
+CCB_ADDRESS = $(CONDOR_HOST)
+
+DAEMON_LIST = MASTER, STARTD
+
+QUEUE_ALL_USERS_TRUSTED = True
+
+START = True
+SUSPEND = False
+PREEMPT = False
+KILL = False
+
+# Machine resource settings
+use feature:PartitionableSlot
+
+USE_SHARED_PORT = True
+
+LOCAL_DIR = /global/cscratch1/sd/tylern/condor/$(HOSTNAME)
+RELEASE_DIR = /global/common/software/m3792/htcondor
+
+SEC_PASSWORD_FILE = /global/homes/t/tylern/condor/cm.password
+SEC_DEFAULT_AUTHENTICATION_METHODS = PASSWORD, FS
+
+ALLOW_READ = 128.55.*, 10.*
+ALLOW_WRITE = 128.55.*, 10.*
+
+SCHEDD_DEBUG = D_FULLDEBUG
+COLLECTOR_DEBUG = D_FULLDEBUG
+NEGOTIATOR_DEBUG = D_FULLDEBUG
+MATCH_DEBUG = D_FULLDEBUG
+SCHEDD_DEBUG = D_FULLDEBUG
+"""
+
+
 if __name__ == '__main__':
     env_vars = {key: os.environ.get(key)
-                for key in ["USERNAME", "PORT", "HOSTNAME", "PASSWORDFILE"]}
+                for key in ["USERNAME", "PORT", "HOSTNAME", "PASSWORDFILE", "CENTRALMANAGER"]}
 
     env_vars['LOCALPASSWORDFILE'] = env_vars['PASSWORDFILE'].split("/")[-1]
 
