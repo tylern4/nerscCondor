45c711ef2c44f6fe28d2876624db0a63228e749d
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-03-03 15:46:40 -0500

    Remove seperate submit server

--- spinConfig/centralmanager/src/app.py
+++ spinConfig/centralmanager/src/app.py
@@ -2,7 +2,7 @@ from crypt import methods
 import htcondor
 import classad
 
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
@@ -12,8 +12,31 @@ def read_root():
             "htcondor": htcondor.__version__}
 
 
-@app.route('/submit', methods=['GET'])
-def submit():
+# @app.route('/hostname', methods=['GET'])
+# def hostname():
+#     hostname_job = htcondor.Submit({
+#         "executable": "/bin/hostname",  # the program to run on the execute node
+#         # anything the job prints to standard output will end up in this file
+#         "output": "/home/submituser/jobs/hostname/outputs/hostname.$(Cluster).$(Process).out",
+#         # anything the job prints to standard error will end up in this file
+#         "error": "/home/submituser/jobs/hostname/outputs/hostname.$(Cluster).$(Process).err",
+#         # this file will contain a record of what happened to the job
+#         "log": "/home/submituser/jobs/hostname/log/hostname.$(Cluster).$(Process).log",
+#         "request_cpus": "1",            # how many CPU cores we want
+#         "request_memory": "128MB",      # how much memory we want
+#         "request_disk": "128MB",        # how much disk space we want
+#     })
+
+#     # get the Python representation of the scheduler
+#     schedd = htcondor.Schedd()
+
+#     submit_result = schedd.submit(hostname_job, count=1)  # submit the job
+
+#     return {"cluster": str(submit_result.cluster())}
+
+
+@app.route('/hostname/<count>', methods=['GET', 'POST'])
+def hostname_count(count):
     hostname_job = htcondor.Submit({
         "executable": "/bin/hostname",  # the program to run on the execute node
         # anything the job prints to standard output will end up in this file
@@ -28,6 +51,49 @@ def submit():
     })
 
     # get the Python representation of the scheduler
+    schedd = htcondor.Schedd()
+
+    submit_result = schedd.submit(hostname_job, count=int(count))  # submit the job
+
+    return {"cluster": str(submit_result.cluster()), "count": count}
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    content = request.get_json(silent=True)
+    if content is None:
+        return {
+            "executable": "",
+            "output": "$(Cluster).$(Process).out",
+            "error": "$(Cluster).$(Process).err",
+            "log": "$(Cluster).$(Process).log",
+            "request_cpus": "1",
+            "request_memory": "128MB",
+            "request_disk": "128MB",
+        }
+
+    has_keys = [content.__contains__(key) for key in ["executable",
+                                                      "output",
+                                                      "error",
+                                                      "log",
+                                                      "request_cpus",
+                                                      "request_memory",
+                                                      "request_disk"]]
+    if sum(has_keys) != 7:
+        return "False"
+
+    hostname_job = htcondor.Submit({
+        "executable": "/bin/hostname",  # the program to run on the execute node
+        # anything the job prints to standard output will end up in this file
+        "output": "/home/submituser/jobs/hostname/outputs/hostname.$(Cluster).$(Process).out",
+        # anything the job prints to standard error will end up in this file
+        "error": "/home/submituser/jobs/hostname/outputs/hostname.$(Cluster).$(Process).err",
+        # this file will contain a record of what happened to the job
+        "log": "/home/submituser/jobs/hostname/log/hostname.$(Cluster).$(Process).log",
+        "request_cpus": "1",            # how many CPU cores we want
+        "request_memory": "128MB",      # how much memory we want
+        "request_disk": "128MB",        # how much disk space we want
+    })
     schedd = htcondor.Schedd()
 
     submit_result = schedd.submit(hostname_job, count=1)  # submit the job
