d70dccde658e984a7ba260576334104ebb30bb16
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-03-21 13:15:10 -0400

    Change owner and premisions

--- spinConfig/centralmanager/pre-exec.sh
+++ spinConfig/centralmanager/pre-exec.sh
@@ -11,5 +11,13 @@ if [[ ! -f "/etc/condor/passwords.d/${PASSWORDFILE}" ]]; then
     cat /etc/condor/passwords.d/${PASSWORDFILE} 
 fi
 
+# If variable is set and the file does not exist make a new "Random" password
+if [[ ! -f "/app/${PASSWORDFILE}" ]]; then
+    cp /etc/condor/passwords.d/${PASSWORDFILE} /app/${PASSWORDFILE}
+    chown submituser:submituser /app/${PASSWORDFILE}
+    cat /app/${PASSWORDFILE} 
+fi
+
+chown -R submituser:submituser /app
 # Write our new configs from environment
 python3 /root/config/startup.py
