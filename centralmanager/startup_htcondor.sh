459a8a0de8eee214e0005ded1fa493ab34a5705a
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-09-20 11:51:24 -0400

    Adding statup scripts

--- centralmanager/startup_htcondor.sh
+++ centralmanager/startup_htcondor.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-# Checks if scratch is valid
 #if [[ $SCRATCH == *"UNKNOWN"* ]]; then
 #    export LOGDIR=$CFS/nstaff/tylern/htcondorscratch
 #else
 #    export LOGDIR=$SCRATCH/htcondorscratch
 #fi
+export LOGDIR=${CFS}/nstaff/tylern/htcondorscratch
 
-export LOGDIR=$CFS/nstaff/$USER/htcondorscratch
+export PORT=9618
+export PASSWORDFILE=${HOME}/.condor/spin.password
+export CONDOR_INSTALL=/global/common/software/m3792/htcondor
+export PATH=${PATH}:${CONDOR_INSTALL}/bin:${CONDOR_INSTALL}/sbin
+export CONDOR_CONFIG=${HOME}/htcondor_workflow_scron/conf_server.conf
+
+python startup.py
+
 echo $LOGDIR
-
 mkdir -p $LOGDIR/$(hostname)/log
 mkdir -p $LOGDIR/$(hostname)/execute
 mkdir -p $LOGDIR/$(hostname)/spool
-
-export CONDOR_INSTALL=/global/common/software/m3792/htcondor
-export PATH=${PATH}:${CONDOR_INSTALL}/bin:${CONDOR_INSTALL}/sbin
-export CONDOR_CONFIG=/global/homes/t/tylern/htcondor_workflow_scron/condor_server.config
 echo $(hostname) > $LOGDIR/currenthost
 
 condor_master
 
-sleep infinity
+sleep 86300
